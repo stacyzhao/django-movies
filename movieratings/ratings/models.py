41c9fbc98dbe714caa650465266ecddc3305c7e6
Stacy Zhao <stacyzhao@hotmail.com>
2016-05-05 18:18:39 -0400

    added migrations

--- movieratings/ratings/models.py
+++ movieratings/ratings/models.py
@@ -1,26 +1,27 @@
 from django.db import models
-
-
-class Rating(models.Model):
-    user_id = models.ForeignKey('Rater', on_delete=models.CASCADE)
-    movie_id = models.ForeignKey('Movie', on_delete=models.CASCADE)
-    rating = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.user_id, self.movie_id, self.rating
-
-
-class Rater(models.Model):
-    user_id = models.ForeignKey(Rating, on_delete=models.CASCADE)
-    age = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.user_id, self.age
+import datetime
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=100)
-    release_date = models.DateTimeField()
+    title = models.CharField(max_length=200)
+    release_date = models.DateField(default=datetime.date.today)
 
-    def __str__(self):
-        return self.title, self.release_date
+    # def __str__(self):
+    #     return self.title, self.release_date
+
+
+class Rater(models.Model):
+    age = models.IntegerField()
+    gender = models.CharField(max_length=1)
+    #
+    # def __str__(self):
+    #     return self.user_id, self.age
+
+
+class Rating(models.Model):
+    rater = models.ForeignKey(Rater, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+
+    # def __str__(self):
+    #     return self.user_id, self.movie_id, self.rating
