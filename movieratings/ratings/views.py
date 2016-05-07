6a6cd34c0c0381ef4b448b129e44ac081ac47537
Stacy Zhao <stacyzhao@hotmail.com>
2016-05-07 19:11:13 -0400

    linked all urls!

--- movieratings/ratings/views.py
+++ movieratings/ratings/views.py
@@ -6,36 +6,43 @@ from django.template import loader
 
 
 def index(request):
-    avg_list = Rating.objects.annotate(avg_rating=Avg('movie_id')).order_by('-average_stars').order_by('-avg')
-    template = loader.get_template('ratings/index.html')
+    # movie = Movie.objects.get(pk=movie_id)
+    movies = Movie.objects.order_by('-avg_rating')[:20]
     context = {
-        'avg_list': avg_list,
+        'movies': movies,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'ratings/index.html', context)
 
 
-def get_movies(request):
-    movie_list = Movie.objects.order_by('-avg_rating')[:20]
+def get_movies(request, movie_id):
+    # movie = Movie.get_object_or_404(Movie, pk=movie_id)
+
+    movie = Movie.objects.get(pk=movie_id)
     # output = ', '.join([q.title for q in movie_list])
     # template = loader.get_template('ratings/movie.html')
-    context = {'movie_list': movie_list}
-    return render(request, 'ratings/movie.html', context)
+    ratings = movie.rating_set.all()
+    return render(request, 'ratings/movie.html', {'movie': movie, 'ratings': ratings})
 
 
-def get_ratings(request):
-    rating_list = Rating.objects.annotate(num_rating=Count('movie_id'))[:10]
-    context = {'rating_list': rating_list}
-    return render(request, 'ratings/rating.html', context)
+# def get_ratings(request):
+#     rating_list = Rating.objects.annotate(num_rating=Count('movie_id'))[:10]
+#     context = {'rating_list': rating_list}
+#     return render(request, 'ratings/rating.html', context)
 
 
-def get_raters(request):
-    rater_list = Rater.objects.order_by('id')
+def get_raters(request, rater_id):
+    rater = Rater.objects.get(pk=rater_id)
+    ratings = rater.rating_set.all()
+
+
+    # rater_list = Rater.objects.order_by('id')
     # output = ', '.join([q.age for q in rater_list])
     # template = loader.get_template('ratings/rating.html')
     context = {
-        'rater_list': rater_list,
+        'rater': rater,
+        'ratings': ratings,
         }
-    return render(request, 'ratings/rating.html', context)
+    return render(request, 'ratings/rater.html', context)
 
 # def getsinglemovie(movie):
     # get movie object = movie
